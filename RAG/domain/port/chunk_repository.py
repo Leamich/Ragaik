8b2d11e3b61d096a7e8cb502816e9837b0768895
Konstantin Anisimov <anisimovkosta19@gmail.com>
2025-05-06 21:53:29 +0300

    Add vector storing strategy to ensemble the storing methods

--- RAG/domain/port/chunk_repository.py
+++ RAG/domain/port/chunk_repository.py
@@ -1,17 +1,21 @@
 from abc import ABC, abstractmethod
 from typing import Any, List
+
+from .vector_storing_strategy import VectorStoringStrategy
 from ..chunk import Chunk
+
 
 class ChunkRepository(ABC):
     """
     Abstract base class for storing and querying chunks.
     """
+
     @abstractmethod
-    def add(self, chunks: List[Chunk]) -> None:
+    def add(self, chunks: List[Chunk], strategies: list[VectorStoringStrategy]) -> None:
         """Add chunks to the store. Implementation defines handling."""
         pass
 
     @abstractmethod
-    def query(self, key: Any, top_k: int) -> List[Chunk]:
+    def query(self, key: any, top_k: int, strategy: list[VectorStoringStrategy]) -> list[list[Chunk]]:
         """Return top_k chunks matching the query key."""
         pass
