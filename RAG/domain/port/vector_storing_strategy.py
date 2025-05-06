22e992679d302a983ec7f444778679b42c7af455
Konstantin Anisimov <anisimovkosta19@gmail.com>
2025-05-06 21:53:29 +0300

    Add vector storing strategy to ensemble the storing methods

---
+++ RAG/domain/port/vector_storing_strategy.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from ..chunk import Chunk
+
+class VectorStoringStrategy(ABC):
+    """Strategy for storing chunks."""
+
+    @abstractmethod
+    def store_vector(self, chunk: Chunk) -> any:
+        pass
+
+    @abstractmethod
+    def query_vector(self, key: any, top_k: int) -> any:
+        pass
+
+
+
+class FaissStoringStrategy(VectorStoringStrategy):
+    pass
+
+class PineconeStoringStrategy(VectorStoringStrategy):
+    pass
