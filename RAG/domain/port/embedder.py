8189a0a7638c7ea3bf5f3cca55c98b56388fa54b
Konstantin Anisimov <anisimovkosta19@gmail.com>
2025-05-09 14:14:40 +0300

    Move RecursiveChunker implementation to another layer

--- RAG/domain/port/embedder.py
+++ RAG/domain/port/embedder.py
@@ -1,7 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List
 
-from transformers import BertTokenizer, TFBertModel
 from torch import FloatTensor
 
 from ..chunk import Chunk
@@ -12,18 +10,8 @@ class Embedder(ABC):
     Abstract port class for generating embeddings from texts.
     Supposed to embed one Document, split into chunks.
     """
+
     @abstractmethod
     def embed(self, chunk: Chunk) -> tuple[FloatTensor]:
         """Embed a list of texts and return corresponding embeddings."""
         pass
-
-
-class MathBERTEmbedder(Embedder):
-    def __init__(self):
-        self._tokenizer = BertTokenizer.from_pretrained('tbs17/MathBERT', output_hidden_states=True)
-        self._model = TFBertModel.from_pretrained('tbs17/MathBERT')
-
-    def embed(self, chunk: Chunk) -> tuple[FloatTensor]:
-        tokens = self._tokenizer(chunk, return_tensors='tf')
-        return self._model(tokens)
-
