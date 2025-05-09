65159704a703baeae413ddad91c337b27ddcb9ce
Konstantin Anisimov <anisimovkosta19@gmail.com>
2025-05-09 14:56:02 +0300

    Added abstract chunk repository implementation.

--- RAG/domain/port/loader.py
+++ RAG/domain/port/loader.py
@@ -1,12 +1,15 @@
 from abc import ABC, abstractmethod
-from typing import Any, List
-from langchain.schema import Document
+from typing import Any
+
+from ..chunk import Document
+
 
 class DocumentLoader(ABC):
     """
     DocumentLoader port class for loading documents from various sources.
     """
+
     @abstractmethod
-    def load(self, source: Any) -> List[Document]:
+    def load(self, source: Any) -> list[Document]:
         """Load documents from the given source and return as a list."""
         pass
--- RAG/infrastructure/chunk_repository/mathbert_embedder.py
+++ RAG/infrastructure/chunk_repository/mathbert_embedder.py
@@ -1,15 +1,14 @@
-from torch import FloatTensor
 from transformers import BertTokenizer, TFBertModel
 
 from domain.chunk import Chunk
-from domain.port.embedder import Embedder
+from infrastructure.chunk_repository.embedder import AbstractEmbedder, Embedding
 
 
-class MathBERTEmbedder(Embedder):
+class MathBERTEmbedder(AbstractEmbedder):
     def __init__(self):
         self._tokenizer = BertTokenizer.from_pretrained('tbs17/MathBERT', output_hidden_states=True)
         self._model = TFBertModel.from_pretrained('tbs17/MathBERT')
 
-    def embed(self, chunk: Chunk) -> tuple[FloatTensor]:
+    def embed(self, chunk: Chunk) -> Embedding:
         tokens = self._tokenizer(chunk, return_tensors='tf')
         return self._model(tokens)
