e2d8640904819ab90097c5d96fe4825fdeb6b707
Konstantin Anisimov <anisimovkosta19@gmail.com>
2025-05-09 14:06:26 +0300

    Move RecursiveChunker implementation to another layer

--- RAG/domain/chunk.py
+++ RAG/domain/chunk.py
@@ -3,9 +3,6 @@ from dataclasses import dataclass
 
 from typing import Any, Dict, List
 
-from langchain.schema import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from transformers import PreTrainedTokenizer
 
 
 # This is more of a value class as it identity is fully described by its content and metadata.
@@ -27,27 +24,5 @@ class Chunker(ABC):
     def chunk(self, document: Document) -> List[Chunk]:
         """Split a Document into a list of Chunk instances."""
         pass
-
-
-class RecursiveChunker(Chunker):
-    """
-    Recursive realization of chunker via given tokenizer.
-    Chunk size = 512 tokens.
-    """
-    def __init__(self, tokenizer: PreTrainedTokenizer) -> None:
-        self._CHUNK_SIZE: int = 512
-        self._tokenizer: PreTrainedTokenizer = tokenizer
-        self._text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-            self._tokenizer,
-            chunk_size=self._CHUNK_SIZE,
-            chunk_overlap=int(self._CHUNK_SIZE / 10),
-            add_start_index=True,
-            strip_whitespace=True,
-        )
-
-    def chunk(self, document: Document) -> List[Chunk]:
-        chunk_as_documents: List[Document] = self._text_splitter.split_documents([document])
-        return [Chunk(chunk.page_content, chunk.metedata) for chunk in chunk_as_documents]
-
 
 
