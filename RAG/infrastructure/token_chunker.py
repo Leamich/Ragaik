8ac1cc157226d2a4433d18850cc5e06346372bbc
ubetu <pavel.seroglazov6@gmail.com>
2025-06-01 12:49:42 +0300

    fix chunker fix

--- RAG/infrastructure/token_chunker.py
+++ RAG/infrastructure/token_chunker.py
@@ -1,4 +1,5 @@
-from langchain.text_splitter import HuggingFaceTokenTextSplitter
+from langchain_text_splitters.base import TokenTextSplitter
+from transformers import AutoTokenizer
 from langchain.schema import Document
 from .chunk_repository.chunker import Chunker
 
@@ -11,17 +12,17 @@ class TokenChunker(Chunker):
 
     def __init__(
         self,
-        tokenizer_name: str = "intfloat/multilingual-e5-large",
+        tokenizer: AutoTokenizer = AutoTokenizer.from_pretrained("intfloat/multilingual-e5-large"),
         chunk_size: int = 400
     ) -> None:
         self._CHUNK_SIZE = chunk_size
-        self._tokenizer_name = tokenizer_name
+        self._tokenizer = tokenizer
 
-        self._text_splitter = HuggingFaceTokenTextSplitter(
-            encoding_name=self._tokenizer_name,
+
+        text_splitter = TokenTextSplitter.from_huggingface_tokenizer(
+            tokenizer=self._tokenizer,
             chunk_size=self._CHUNK_SIZE,
             chunk_overlap=int(self._CHUNK_SIZE / 10),
-            model_name=self._tokenizer_name
         )
 
     def chunk(self, document: Document) -> list[Document]:
