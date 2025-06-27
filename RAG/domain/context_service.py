0d5716a88b43f2b51c25f05a05f71d105d77a6b8
ubetu <pavel.seroglazov6@gmail.com>
2025-06-27 19:42:50 +0300

    remove loaders

--- RAG/domain/context_service.py
+++ RAG/domain/context_service.py
@@ -19,25 +19,11 @@ class ContextService:
 
     def __init__(
         self,
-        notes_loader: DocumentLoader,  # TODO add loader
-        photos_loader: DocumentLoader,  # TODO add loader
         notes_repository=FaissAndBM25EnsembleRetriever(),
         photos_repository=BM25ChunkRepository(),
     ):
-        self._notes_loader = notes_loader
-        self._photos_loader = photos_loader
         self._notes_repository = notes_repository
         self._photos_repository = photos_repository
-
-    def ingest_notes(self, source: Any) -> None:
-        """Load notes, and add to the datastore."""
-        notes = self._notes_loader.load(source)
-        self._notes_repository.add_batch(notes)
-
-    def ingest_photos(self, source: Any) -> None:
-        """Load documents, made from photos, and add to the datastore."""
-        photos = self._photos_loader.load(source)
-        self._photos_repository.add_batch(photos)
 
     def retrieve_textual_context(self, query: str) -> Context:
         """
